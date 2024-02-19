ea621dd06d8ae770d8e592d5c8b9b5a27fa2d4a6
Valentin Hippolyte <valenthippo@gmail.com>
2024-02-19 15:24:28 +0100

    upload images with vich bundle works

--- src/Form/GameType.php
+++ src/Form/GameType.php
@@ -4,20 +4,24 @@ namespace App\Form;
 
 use App\Entity\Game;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class GameType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('name')
             ->add('description')
-        ;
+            ->add('imageFile', FileType::class, [
+                'label' => 'Upload Image',
+                'required' => false, // Vous pouvez définir cette option selon vos besoins
+            ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Game::class,
